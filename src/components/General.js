5336137cc8c2a8a2f4e0d8b96d85188b2fb86d59
Elena Kashtanova <lenachestnut@gmail.com>
2020-12-14 19:37:02 +0300

    Edit styles

--- src/components/General.js
+++ src/components/General.js
@@ -23,7 +23,7 @@ class General extends React.Component {
 
         return (
             <div className="section general">
-                {/* <div className="section__info">
+                <div className="section__info">
                     <h1 className="general__name">Елена Каштанова</h1>
                     <h2 className="section__title">Contact info</h2>
                     <p className="general__email">
@@ -35,8 +35,8 @@ class General extends React.Component {
                     <div className="section__buttons">
                         <Edit className="section__icon"/>
                     </div>
-                </div> */}
-                <form className="section__form">
+                </div>
+                {/* <form className="section__form">
                     <input
                         type="text"
                         className="form__name-input"
@@ -72,7 +72,7 @@ class General extends React.Component {
                         <button type="submit"><CheckSquare className="section__icon"/></button>
                         <button type="button"><XSquare className="section__icon"/></button>
                     </fieldset>
-                </form>
+                </form> */}
             </div>
         )
     }

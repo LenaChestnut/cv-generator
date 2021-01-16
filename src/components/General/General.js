c9281e442131260f699dae407bf889347c799eac
Elena Kashtanova <lenachestnut@gmail.com>
2021-01-16 17:24:26 +0300

    Add default view

--- src/components/General/General.js
+++ src/components/General/General.js
@@ -3,41 +3,37 @@ import EditButton from './../shared/EditButton';
 // import GeneralForm from './GeneralForm';
 
 class General extends React.Component {
-    // constructor() {
-    //     super();
+    constructor() {
+        super();
 
-    //     this.state = {
-    //         email: '',
-    //         phone: '',
-    //         fullName: ''
-    //     }
-
-    //     this.handleChange = this.handleChange.bind(this);
-    // }
-
-    // handleChange(e) {
-    //     this.setState({[e.target.name]: e.target.value});
-    // }
+        this.state = {
+            fullName: '',
+            email: '',
+            phone: ''
+        }
+    }
 
     render() {
+        const { fullName, email, phone } = this.state;
+
         return (
             <section className="section">
                 <div className="section__info">
-                    <h1 className="name">John Doe</h1>
+                    <h1 className="name">{fullName ? fullName : "Your Name"}</h1>
                     <h2 className="section__title">Contact info</h2>
                     <div className="info-item info-item-general">
                         <p className="info-item__extra email">
-                            <span className="label">Email:</span> lenachestnut@gmail.com
+                            <span className="label">Email:</span> {email ? email : "YourEmail@gmail.com"}
                         </p>
                         <p className="info-item__extra phone">
-                            <span className="label">Phone:</span> +7927555555
+                            <span className="label">Phone:</span> {phone ? phone : "5-555-555"}
                         </p>
                     </div>
+                    <EditButton />
                 </div>
-                <EditButton />
                 {/* <GeneralForm /> */}
             </section>
-        )
+        );
     }
 }
 

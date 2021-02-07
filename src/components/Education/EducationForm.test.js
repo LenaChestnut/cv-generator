9718d07bb16a935a5136c9c3afe4d6922b808f59
Elena Kashtanova <lenachestnut@gmail.com>
2021-02-07 14:14:41 +0300

    Create EducationForm test file

---
+++ src/components/Education/EducationForm.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import EducationForm from './EducationForm';
+
+describe('Form is rendered correctly', () => {
+    test.todo('Constant elements are rendered');
+    test.todo('With no existing info');
+    test.todo('With existing info');
+});
+
+test.todo('Inputs accept changes');
+
+test.todo('Form is closed on Cancel click');
+
+describe('Form is removed on Delete click', () => {
+    test.todo('When form is empty');
+    test.todo('When info already existed');
+});
+
+describe('Form submitted', () => {
+    test.todo('With valid inputs');
+    test.todo('With invalid inputs');
+})

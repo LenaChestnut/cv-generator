3a6d8ff56607d43e89113128453b6bb4b0431bff
Elena Kashtanova <lenachestnut@gmail.com>
2021-02-07 10:37:57 +0300

    Remove extra code

--- src/components/General/General.test.js
+++ src/components/General/General.test.js
@@ -2,8 +2,6 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import General from './General';
 
-const mockSaveInfo = jest.fn();
-
 const testEmptyInfo = {
     fullName: '',
     email: '',

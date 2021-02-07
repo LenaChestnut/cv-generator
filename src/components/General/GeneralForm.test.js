26a2d4188859fdc51c1d2f8950c9187f24c03969
Elena Kashtanova <lenachestnut@gmail.com>
2021-02-07 12:30:21 +0300

    Test submit event with invalid email

--- src/components/General/GeneralForm.test.js
+++ src/components/General/GeneralForm.test.js
@@ -5,8 +5,14 @@ import GeneralForm from './GeneralForm';
 
 const mockHandleSubmit = jest.fn();
 
+const testInfo = {
+    fullName: '',
+    email: '',
+    phone: '',
+}
+
 test('Form input values change on type', () => {
-    render(<GeneralForm />);
+    render(<GeneralForm info={testInfo}/>);
     userEvent.type(screen.getByLabelText(/fullName/i), 'John Doe');
     userEvent.type(screen.getByLabelText(/email/i), 'email@test.com');
     userEvent.type(screen.getByLabelText(/phone/i), '123-456');
@@ -15,9 +21,9 @@ test('Form input values change on type', () => {
     expect(screen.getByLabelText(/phone/i)).toHaveValue('123-456');
 });
 
-describe.only('Form submitted', () => {
+describe('Form submitted', () => {
     beforeEach(() => {
-        render(<GeneralForm handleFormSubmit={mockHandleSubmit} />);
+        render(<GeneralForm handleFormSubmit={mockHandleSubmit} info={testInfo}/>);
     });
 
     describe('With valid inputs', () => {
@@ -29,4 +35,12 @@ describe.only('Form submitted', () => {
             expect(mockHandleSubmit).toHaveBeenCalledTimes(1);
         });
     });
+
+    describe('With invalid inputs', () => {
+        test('onSubmit is not called', () => {
+            userEvent.type(screen.getByLabelText(/email/i), 'emailtest.com');
+            userEvent.click(screen.getByRole('button', {name: /submit/i}));
+            expect(screen.getByRole('form')).toBeInTheDocument();
+        })
+    })
 });

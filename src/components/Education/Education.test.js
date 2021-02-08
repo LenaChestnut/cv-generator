752d35b9286576c7f17d9e7011f24b927f07821f
Elena Kashtanova <lenachestnut@gmail.com>
2021-02-08 11:27:57 +0300

    Test basic render

--- src/components/Education/Education.test.js
+++ src/components/Education/Education.test.js
@@ -1,3 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Education from './Education';
+
+const testEmptyInfo = {
+    schools: [],
+}
+
+describe.only('Component is rendered correctly', () => {
+    test('Constant elements are rendered', () => {
+        render(<Education education={testEmptyInfo}/>);
+        expect(screen.getByText(/education/i)).toBeInTheDocument();
+        expect(screen.getByRole('button', {name: /add/i})).toBeInTheDocument();
+    });
+    test.todo('Rendered with default info');
+    test.todo('Rendered with custom info');
+})
+
+test.todo('A form is added after Add is clicked');
+
+test.todo('A form is displayed on Edit click');

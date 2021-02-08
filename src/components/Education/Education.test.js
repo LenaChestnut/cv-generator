264cbb4dcd15010e07765424d1aeba627269576c
Elena Kashtanova <lenachestnut@gmail.com>
2021-02-08 12:29:09 +0300

    Test Education section with custom info

--- src/components/Education/Education.test.js
+++ src/components/Education/Education.test.js
@@ -6,16 +6,47 @@ const testEmptyInfo = {
     schools: [],
 }
 
+const testCustomInfo = {
+    schools: [
+        {
+            id: 1,
+            degree: 'Magic',
+            schoolName: 'Hogwarts',
+            startDate: '2007',
+            endDate: '2014'
+        },
+        {
+            id: 2,
+            degree: 'Computer Science',
+            schoolName: 'Internet',
+            startDate: '2019',
+            endDate: 'present'
+        }
+    ],
+}
+
 describe.only('Component is rendered correctly', () => {
-    test('Constant elements are rendered', () => {
+    test('Default view is rendered', () => {
         render(<Education education={testEmptyInfo}/>);
         expect(screen.getByText(/education/i)).toBeInTheDocument();
         expect(screen.getByRole('button', {name: /add/i})).toBeInTheDocument();
     });
-    test.todo('Rendered with default info');
-    test.todo('Rendered with custom info');
+
+    test('Rendered with custom info', () => {
+        render(<Education education={testCustomInfo}/>);
+        testCustomInfo.schools.forEach(infoItem => {
+            for (const [key, value] of Object.entries(infoItem)) {
+                if (key === 'id') {
+                    return false;
+                }
+                expect(screen.getByText(value)).toBeInTheDocument();
+            }
+        })
+    });
 })
 
 test.todo('A form is added after Add is clicked');
 
 test.todo('A form is displayed on Edit click');
+
+test.todo('Info section is rendered with default info');

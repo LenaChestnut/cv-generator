3e129ba45f1e60cd279b662fe8fd9ad662ee6392
Elena Kashtanova <lenachestnut@gmail.com>
2021-02-08 11:23:22 +0300

    Add button attributes

--- src/components/shared/AddButton.js
+++ src/components/shared/AddButton.js
@@ -4,8 +4,14 @@ import { PlusSquare } from 'react-feather';
 class AddButton extends React.Component {
     render() {
         return (
-            <div className="button-container">
-                <button type="button"><PlusSquare className="button__icon"/></button>
+            <div className='button-container'>
+                <button type='button' aria-label='Add' title='Add'>
+                    <PlusSquare
+                        className='button__icon'
+                        aria-hidden='true'
+                        focusable='false'
+                    />
+                </button>
             </div>
         )
     }

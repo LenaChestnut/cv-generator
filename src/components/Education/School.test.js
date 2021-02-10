12316bd32a55ffe85e25d80282be16d804a0d8b3
Elena Kashtanova <lenachestnut@gmail.com>
2021-02-10 12:04:43 +0300

    Write form test

--- src/components/Education/School.test.js
+++ src/components/Education/School.test.js
@@ -6,6 +6,7 @@ const mockSchoolInfo = {};
 
 test('A form is displayed on Edit click',() => {
     render(<School school={mockSchoolInfo}/>);
+    expect(screen.queryByRole('form')).not.toBeInTheDocument();
     userEvent.click(screen.getByRole('button', {name: /edit/i}));
     expect(screen.getByRole('form')).toBeInTheDocument();
 });

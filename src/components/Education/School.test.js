ff5125ab66de40fc6270edd9155e8c2004062d06
Elena Kashtanova <lenachestnut@gmail.com>
2021-02-10 12:00:10 +0300

    Write test for form display on Edit click

---
+++ src/components/Education/School.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import School from './School';
+
+const mockSchoolInfo = {};
+
+test('A form is displayed on Edit click',() => {
+    render(<School school={mockSchoolInfo}/>);
+    userEvent.click(screen.getByRole('button', {name: /edit/i}));
+    expect(screen.getByRole('form')).toBeInTheDocument();
+});

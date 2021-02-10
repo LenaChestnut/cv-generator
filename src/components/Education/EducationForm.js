76764faa12ade9baa7b8bcbd02c9b7dceb52effc
Elena Kashtanova <lenachestnut@gmail.com>
2021-02-10 12:46:07 +0300

    Display school form on Edit click

--- src/components/Education/EducationForm.js
+++ src/components/Education/EducationForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import DateInput from "../shared/DateInput";
+import DateInput from '../shared/DateInput';
 import FormButtons from './../shared/FormButtons';
 
 class EducationForm extends React.Component {
@@ -7,19 +7,19 @@ class EducationForm extends React.Component {
         const { schoolName, degree, startDate, endDate } = this.props.school;
 
         return (
-            <form className="form">
+            <form className='form' name='school-form'>
                 <input
-                    type="text"
-                    className="form__degree-input"
-                    name="degree"
-                    placeholder="Degree title"
+                    type='text'
+                    className='form__degree-input'
+                    name='degree'
+                    placeholder='Degree Title'
                     value={degree}
                     onChange={this.handleChange}
                 ></input>
                 <input 
-                    type="text"
-                    name="school"
-                    placeholder="School name"
+                    type='text'
+                    name='school'
+                    placeholder='School Name'
                     value={schoolName}
                     onChange={this.handleChange}
                 ></input>
--- src/components/Education/School.js
+++ src/components/Education/School.js
@@ -1,20 +1,40 @@
 import React from "react";
 import EditButton from './../shared/EditButton';
-// import EducationForm from './EducationForm';
+import EducationForm from './EducationForm';
 
 class School extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isFormOpen: false,
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(state => ({
+            isFormOpen: !state.isFormOpen,
+        }));
+    }
+
     render() {
         const { degree, schoolName, startDate, endDate } = this.props.school;
 
         return (
             <div className="info-item">
-                <div>
-                    <h3 className="info-item__main degree">{degree ? degree : 'Degree Title'}</h3>
-                    <p className="info-item__extra school-name">{schoolName ? schoolName : 'School Name'}</p>
-                    <p className="info-item__extra dates">{startDate ? startDate : 'XXXX'} - {endDate ? endDate : 'XXXX'}</p>
-                    <EditButton />
-                </div>
-                {/* <EducationForm school={this.props.school} /> */}
+                {
+                    this.state.isFormOpen
+                    ? <EducationForm school={this.props.school} />
+                    :
+                    <div>
+                        <h3 className="info-item__main degree">{degree ? degree : 'Degree Title'}</h3>
+                        <p className="info-item__extra school-name">{schoolName ? schoolName : 'School Name'}</p>
+                        <p className="info-item__extra dates">{startDate ? startDate : 'XXXX'} - {endDate ? endDate : 'XXXX'}</p>
+                        <EditButton handleClick={this.handleClick}/>
+                    </div>
+                }
             </div>
         )
     }

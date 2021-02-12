d954622003539d1e9d124f59ee82fd94cc1ba2c7
Elena Kashtanova <lenachestnut@gmail.com>
2021-02-12 13:00:38 +0300

    Create mock data

---
+++ src/components/Experience/mockExperienceData.js
@@ -0,0 +1,24 @@
+export const mockEmptyExperience = {
+    workplaces: [],
+};
+
+export const mockExperience = {
+    workplaces: [
+        {
+            id: '123',
+            jobTitle: 'Auror',
+            companyName: 'Ministry of Magic',
+            jobDescription: 'Fighting the forces of dark magic',
+            startDate: '2009',
+            endDate: '2015',
+        },
+        {
+            id: '456',
+            jobTitle: 'Wandering Wizard',
+            companyName: 'Freelance',
+            jobDescription: 'Motivating hobbits to go on an adventure',
+            startDate: '2015',
+            endDate: 'present time',
+        },
+    ]
+};
